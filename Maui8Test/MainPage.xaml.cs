03ec7cd077c3bb5e3bbc4ab3f02b8b9d5f2af90d
ipax77 <ph.ilipp@web.de>
2023-05-17 13:00:11 +0200

    MAUI dotnet 8 preview 4 test

---
+++ Maui8Test/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Maui8Test
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+
+[https://github.com/dotnet/maui/issues/9216](https://github.com/dotnet/maui/issues/9216)
